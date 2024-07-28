154db6f96266c8d41fe1ff63dd6908a066137ee2
Aolin Chen <aolin.chen.cs@gmail.com>
2024-07-27 21:13:23 -0700

    add 4 bugs

--- chatrepair.sh
+++ chatrepair.sh
@@ -2,5 +2,5 @@ projects=("Lang" "Chart" "Closure" "Math" "Mockito" "Time")
 for project in "${projects[@]}"
 do
     echo "Runing chatrepair............."$project""
-    python3 main.py chatrepair "$project" n
+    python3 main.py chatrepair "$project" y
 done
